9b2c73358c8c698e48a2c2dceac836c26ef6faca
Sushil97 <sushil500530@gmail.com>
2023-11-24 17:51:29 +0600

    token generate added, and user added

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware 
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174"
+    ]
+}));
+app.use(express.json());
+
+
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.ruakr2a.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -22,10 +32,20 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // all collection here 
-        const userCollection = client.db('inventoryDB').collection('users');
+        const usersCollection = client.db('inventoryDB').collection('users');
 
 
-
+        // jwt related api 
+        app.post('/jwt', async (req, res) => {
+            try {
+                const user = req.body;
+                const token = jwt.sign(user, process.env.ACCEASS_TOKEN_SECRET, { expiresIn: '365d' });
+                res.send({ token })
+            }
+            catch (err) {
+                console.log(err);
+            }
+        })
 
 
 
@@ -33,7 +53,7 @@ async function run() {
         app.get('/users', async (req, res) => {
             try {
 
-                const result = await userCollection.find().toArray();
+                const result = await usersCollection.find().toArray();
                 res.send(result)
             }
             catch (error) {
@@ -41,9 +61,21 @@ async function run() {
             }
         })
 
-
-
-
+        app.post('/users', async (req, res) => {
+            try {
+                const user = req.body;
+                const query = { email: user.email };
+                const existingUser = await usersCollection.findOne(query);
+                if (existingUser) {
+                    return res.send({ message: 'user already exists', insertedId: null })
+                }
+                const result = await usersCollection.insertOne(user);
+                res.send(result)
+            }
+            catch (error) {
+                console.log(error);
+            }
+        })
 
 
         // Send a ping to confirm a successful connection
